eea795b6d732531996cd41da82934c4a590ae800
aaronfavc <aaron.spencer@posadas.com>
2020-11-06 17:44:23 -0600

    update

--- Cobranza/jss/anualidad.js
+++ Cobranza/jss/anualidad.js
@@ -1,17 +1,24 @@
 function anualidad() {
-    let puntos = document.getElementById('input1').value;
-    let type = parseInt(document.getElementById('input2').value);
-    let status = parseInt(document.getElementById('input3').value);
+    let puntos = parseInt(document.getElementById('input1').value);
+    let type = document.getElementById('input2').value;
+    let status = document.getElementById('input3').value;
 
 
     if (type == "Regular") {
-        let x = 318;
+        let x = 318.00;
         if (status == "Regular") {
-            let y = .1952;
-            let cuota = x;
-            let mtto = (puntos * y);
-            document.getElementById('output1').innerHTML = cuota;
-            document.getElementById('output2').innerHTML = mtto; 
+            let y = .1952 * puntos;
+            let cuota = x.toFixed(2);
+            let mtto = y.toFixed(2);
+            let usd = cuota + mtto;
+            let answer = formatNumber(usd)
+            let fix1 = answer;
+            let mxn = usd * 20;
+            let fix2 = formatNumber(mxn);
+            document.getElementById('output1').innerHTML = "Cuota: $" + cuota + " USD";
+            document.getElementById('output2').innerHTML = "MTTO: $" + mtto + " USD";
+            document.getElementById('output3').innerHTML = "Total: $" + fix1 + " USD";
+            document.getElementById('output4').innerHTML = "Total: $" + fix2 + " MXN";
         }
         if (status == "Founder") {
             y = .1632 
@@ -27,3 +34,6 @@ function anualidad() {
         } 
     }
 }
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
